e8ac192f6e5e5bc6ee782500926b3eb4b14d7adb
wojtekpi <wojtekpietrucha@gmail.com>
2022-12-27 23:29:03 +0100

    Show support for  different types of tests

--- .projenrc.js
+++ .projenrc.js
@@ -7,5 +7,13 @@ const project = new typescript.TypeScriptProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  jestOptions: {
+    testTypes: {
+      unit: '**/**.unit.test.(ts|js)',
+      component: '**/**.component.test.(ts|js)',
+      integration: '**/**.integration.test.(ts|js)',
+    },
+  },
 });
 project.synth();
